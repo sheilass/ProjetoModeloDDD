5f0b285963702fb0538e73de2157a2f2d67ea2e8
Sheila Santos Souza <sheila.souza@ewave.com.br>
2021-04-19 16:45:23 -0400

    adicionando classe de cliente

---
+++ 3 - Domain/ProjetoModeloDDD.Domain/Entities/Cliente.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    public class Cliente
+    {
+        //chave primária
+        public int ClienteId { get; set; }
+        
+        public string Nome { get; set; }
+       
+        public string Sobrenome { get; set; }
+        
+        public int Email { get; set; }
+        
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        //regra de negócio: valida cliente especial
+        public bool ClienteEspecial (Cliente cliente)
+        {
+            //verifica se cliente está ativo e tempo de serviço é maior ou igual a 5
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+        }
+    }
+}
