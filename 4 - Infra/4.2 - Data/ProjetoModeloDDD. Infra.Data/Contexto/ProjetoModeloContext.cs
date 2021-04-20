26ab72b156c825eeb4ee0e9fc1db1279be5fb47b
Sheila Santos Souza <sheila.souza@ewave.com.br>
2021-04-19 20:33:56 -0400

    Adição de Migrations no projeto e connectionStrings

---
+++ 4 - Infra/4.2 - Data/ProjetoModeloDDD. Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -0,0 +1,20 @@
+﻿
+
+using ProjetoModeloDDD.Domain.Entities;
+using System.Data.Entity;
+
+namespace ProjetoModeloDDD.Infra.Data.Contexto
+{
+    //adiciona o dbcontext, instala o migrations e depois colocar a string de conexão
+    public class ProjetoModeloContext : DbContext        
+    {
+        //construtor para linkar com a string de conexão
+        public ProjetoModeloContext()
+            : base("ProjetoModeloDDD")
+        {
+        }
+
+        //tabela a ser criada
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
