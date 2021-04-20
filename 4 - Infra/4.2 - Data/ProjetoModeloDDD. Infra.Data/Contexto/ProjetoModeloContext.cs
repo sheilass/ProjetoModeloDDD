08481008368b02e502cc13e0046f39af7acf22e6
Sheila Santos Souza <sheila.souza@ewave.com.br>
2021-04-19 21:17:13 -0400

    Configurando os campos do banco de dados

--- 4 - Infra/4.2 - Data/ProjetoModeloDDD. Infra.Data/Contexto/ProjetoModeloContext.cs
+++ 4 - Infra/4.2 - Data/ProjetoModeloDDD. Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -1,7 +1,11 @@
 ﻿
 
 using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Infra.Data.EntityConfig;
+using System;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
 
 namespace ProjetoModeloDDD.Infra.Data.Contexto
 {
@@ -16,5 +20,56 @@ namespace ProjetoModeloDDD.Infra.Data.Contexto
 
         //tabela a ser criada
         public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // tira a convenção que coloca os nomes no plural
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            
+            //para não deletar em cascata quando tiver a relação de um para muitos
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            //para não deletar em cascata quando tiver a relação de muitos para muitos
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //toda vez que um dado for criado e tiver uma palavra com final Id, vai ser uma chave primária
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            // todo dado criado como string será definido como varchar no banco e não nvarchar
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            // define o tamanho da string para um tamanho máximo de 100
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+            //o contexto vai entender que ele deve obedecer o que está definido na classe
+            modelBuilder.Configurations.Add(new ClienteConfiguration());
+        }
+
+
+        //para não precisar ficar continuamente adicionando o valor do DATETIME
+        public override int SaveChanges()
+        {
+
+            foreach (var entry in ChangeTracker.Entries().Where( entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            { 
+                
+                if(entry.State == EntityState.Added) 
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+
+            }
+
+            return base.SaveChanges();
+        }
     }
 }
---
+++ 4 - Infra/4.2 - Data/ProjetoModeloDDD. Infra.Data/EntityConfig/ClienteConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+
+
+namespace ProjetoModeloDDD.Infra.Data.EntityConfig
+{
+    public class ClienteConfiguration : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguration()
+        {
+            //define como chave primária
+            HasKey(c => c.ClienteId);
+
+            //coloca como required e com tamanhpo máximo de 150
+            Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Sobrenome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Email)
+                .IsRequired();
+
+
+        }
+    }
+}
